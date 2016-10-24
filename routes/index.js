240056f5b5fe461c4f746fe60387e145ce886fcb
Xavier Xinwei Wang <xiw180@ucsd.edu>
2016-10-24 13:09:50 -0700

    initialize 2 pages

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET storage page. */
+router.get('/storage', function(req, res, next) {
+  res.render('storage', { title: 'Express' });
+});
+
 module.exports = router;
