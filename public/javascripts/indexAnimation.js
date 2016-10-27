96177d950e4bf4c565720819da0e30d9642c803c
Chen Liu <liuchen1701@gmail.com>
2016-10-27 13:17:44 -0700

    All-Clothes without animation

--- public/javascripts/indexAnimation.js
+++ public/javascripts/indexAnimation.js
@@ -1,33 +1,15 @@
-/**
- * Created by XavierXinweiWang on 16/10/1.
- */
-$('#tight').click(function()
-{
-    $('#header').css("background-image", "url(images/tight.jpg)");
-    $('#header').css("background-position-y", "-12vw");
-    $("#toolbar").attr('class', 'mdl-layout__tab-bar mdl-js-ripple-effect mdl-color--primary-dark red');
-    $("#query").attr('class', 'large material-icons red');
-    $("#help").attr('class', 'mdl-cell--3-col btn-large red');
-    color_mode = 0;
+$('button.more').click(function() {
+
+    $(this).text("LESS");
+
 });
 
-$('#slacky').click(function()
-{
-    $('#header').css("background-image", "url(images/lazycover.jpg)");
-    $('#header').css("background-position-y", "-27vw");
-    $("#toolbar").attr('class', 'mdl-layout__tab-bar mdl-js-ripple-effect mdl-color--primary-dark lime lighten-2');
-    $("#query").attr('class', 'large material-icons green');
-    $("#help").attr('class', 'mdl-cell--3-col btn-large lime lighten-2');
-    color_mode = 2;
-});
 
-$('#mediocre').click(function()
-{
-    $('#header').css("background-image", "url(images/cover.jpg)");
-    $('#header').css("background-position-y", "-25vw");
-    $("#toolbar").attr('class', 'mdl-layout__tab-bar mdl-js-ripple-effect mdl-color--primary-dark orange accent-2');
-    $("#query").attr('class', 'large material-icons yellow darken-1');
-    $("#help").attr('class', 'mdl-cell--3-col btn-large orange accent-2');
-    color_mode = 1;
-});
 
+
+$('.image.less').click(function() {
+    $(".new").hide();
+    $(".image.more").show();
+    $(".image.less").hide();
+
+});
