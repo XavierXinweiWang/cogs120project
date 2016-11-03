4e9a9860191a813df5dc2f89ecc4095f25c3f967
Chen Liu <liuchen1701@gmail.com>
2016-11-03 04:55:48 -0700

    Implemented select and add, but the collapse/expand part not working

--- public/javascripts/indexAnimation.js
+++ public/javascripts/indexAnimation.js
@@ -14,7 +14,6 @@ var new_look = {
 
 var selected_clothes = [];
 
-
 $('#time').click(function()
 {
     $("#toolbar").attr('class', 'mdl-layout__tab-bar mdl-js-ripple-effect mdl-color--primary-dark red');
@@ -59,8 +58,16 @@ $('.card-image').click(function() {
     $(this).find('i').toggle();
 
     if($(this).find('i').attr("style") == "display: none;") {
+      for (var i = 0; i < selected_clothes.length; i++) {
+        if (selected_clothes[i].title == $(this).find('p').text()) {
+          selected_clothes.splice(i, 1);
+        }
+      }
       selected--;
     } else {
+      selected_clothes.push(
+        {"title" : $(this).find('p').text(), "imageURL" : $(this).find('img').attr("src")}
+      );
       selected++;
     }
 
@@ -69,4 +76,24 @@ $('.card-image').click(function() {
     } else {
       $('#filter').text("done");
     }
+});
+
+$('#filter').click(function() {
+  if($(this).text() == "done") {
+    var title = "";
+    for (var i = 0; i < selected_clothes.length; i++) {
+      title = title.concat(selected_clothes[i].title);
+      if(i < selected_clothes.length - 1) {
+        title = title.concat(", ");
+      }
+    }
+    new_look.title = title;
+
+    new_look.imageURL1 = selected_clothes[0].imageURL;
+    new_look.imageURL2 = selected_clothes[1].imageURL;
+
+    $("#history").prepend(
+      "<ul class='collapsible' data-collapsible='accordion'><li id='data1'><div class='collapsible-header'><section class='section--center mdl-grid mdl-grid--no-spacing'><div class='mdl-card mdl-cell--12-col'><div class='mdl-card mdl-cell mdl-cell--12-col' id='form'><form class='col s12'><div class='row'><div class='input-field col l6 m6 s6'><img src='" + new_look.imageURL1 + "' class='fakeitem1' id='fake1'></div><div class='input-field col l6 m6 s6'><p>" + new_look.title + "</p><p id='lastwear1'><i class='material-icons'>access_time</i>" + new_look.date + "</p><p id='like1'><i class='material-icons'>favorite</i>" + new_look.like + "likes</p></div></div></form></div></div></section></div><div class='collapsible-body'><section class='section--center mdl-grid mdl-grid--no-spacing'><div class='mdl-card mdl-cell--12-col'><div class='mdl-card mdl-cell mdl-cell--12-col'><form class='col s12'><div class='row'><div class='input-field col l3 m3 s3'></div><div class='input-field col l3 m3 s3' id = 'fakedata1a'><img src='" + new_look.imageURL1 + "' class='fakeitem2'></div><div class='input-field col l3 m3 s3' id = 'fakedata1b'><img src='" + new_look.imageURL2 + "' class='fakeitem2'></div><div class='input-field col l6 m6 s6'><a class='mdl-button mdl-js-button mdl-button--accent delete' style='width: 100%' id='delete1'>DELETE</a></div><div class='input-field col l6 m6 s6'><a class='mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent wear' style='width: 100%' id='wear1'>WEAR</a></div></div></form></div></div></section></div></li></ul>"
+    );
+  }
 });
