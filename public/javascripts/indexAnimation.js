06a2a9819b818029f81035641f7f6c4e5f84072d
Chen Liu <liuchen1701@gmail.com>
2016-11-03 01:56:08 -0700

    Implemented the FAB in different screen

--- public/javascripts/indexAnimation.js
+++ public/javascripts/indexAnimation.js
@@ -1,6 +1,10 @@
 /**
  * Created by XavierXinweiWang on 16/10/26.
  */
+
+var selected = 0;
+
+
 $('#time').click(function()
 {
     $("#toolbar").attr('class', 'mdl-layout__tab-bar mdl-js-ripple-effect mdl-color--primary-dark red');
@@ -25,6 +29,34 @@ $('.wear').click(function () {
     $(this).parentsUntil("#data1").parent().find('#lastwear1').text(' Today');
 });
 
+$('#to-saved-looks').click(function() {
+  $('#filter').parent().parent().show();
+  $('#filter').text("sort");
+  $('#filter').parent().parent().find("ul").show();
+});
+
+$('#to-all-clothes').click(function() {
+  $('#filter').parent().parent().show();
+  $('#filter').text("filter_list");
+  $('#filter').parent().parent().find("ul").hide();
+});
+
+$('#to-explore').click(function() {
+  $('#filter').parent().parent().hide();
+});
+
 $('.card-image').click(function() {
     $(this).find('i').toggle();
+
+    if($(this).find('i').attr("style") == "display: none;") {
+      selected--;
+    } else {
+      selected++;
+    }
+
+    if (selected == 0) {
+      $('#filter').text("filter_list");
+    } else {
+      $('#filter').text("done");
+    }
 });
