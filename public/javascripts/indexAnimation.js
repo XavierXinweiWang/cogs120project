90731ccc6edcaf1fdf9ab93ce5a4b6f93c1ad617
Chen Liu <liuchen1701@gmail.com>
2016-11-03 00:51:07 -0700

    Re-Enable select clothes to view, delete, and wear to all saved-looks

--- public/javascripts/indexAnimation.js
+++ public/javascripts/indexAnimation.js
@@ -13,50 +13,14 @@ $('#likes').click(function()
     $("#filter").attr('class', 'large material-icons green');
 });
 
-$('#fakedata1a').click(function(){
-    $(this).parentsUntil("#data1").parent().find("#fake1").attr('src', $("img", this).attr("src"));
+$('.fakeitem2').click(function(){
+    $(this).parentsUntil("#data1").parent().find("#fake1").attr('src', $(this).attr("src"));
 });
 
-$('#fakedata1b').click(function(){
-    $('#fake1').attr('src', $("img", this).attr("src"));
+$('.delete').click(function () {
+    $(this).parentsUntil("#data1").parent().parent().remove();
 });
 
-$('#fakedata2a').click(function(){
-    $('#fake2').attr('src','images/2878289015_6_1_1.jpg');
-});
-
-$('#fakedata2b').click(function(){
-    $('#fake2').attr('src','images/hlw8n1ufuhk40yvuq0x0xltssuw3b2tg.jpg');
-});
-
-$('#fakedata3a').click(function(){
-    $('#fake3').attr('src','images/4644272507_6_1_1.jpg');
-});
-
-$('#fakedata3b').click(function(){
-    $('#fake3').attr('src','images/coralfront-1000x1000.jpg');
-});
-
-$('#delete1').click(function () {
-    $('#data1').remove();
-});
-
-$('#delete2').click(function () {
-    $('#data2').remove();
-});
-
-$('#delete3').click(function () {
-    $('#data3').remove();
-});
-
-$('#wear1').click(function () {
-    $('#lastwear1').text(' Today');
-});
-
-$('#wear2').click(function () {
-    $('#lastwear2').text(' Today');
-});
-
-$('#wear3').click(function () {
-    $('#lastwear3').text(' Today');
+$('.wear').click(function () {
+    $(this).parentsUntil("#data1").parent().find('#lastwear1').text(' Today');
 });
